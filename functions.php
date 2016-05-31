b52ee3da0fe9082e1bdb6e88d0458dad2f0e6caa
Connor Gaughan <connor@clubhousecreative.com>
2016-05-31 19:18:58 -0400

    Footer Updates

--- functions.php
+++ functions.php
@@ -7,16 +7,26 @@ class Makespace {
 	}
 
 	function wp_enqueue_scripts(){
-		wp_enqueue_script( 'theme', get_stylesheet_directory_uri() . '/js/theme.js' );
+
+		wp_enqueue_script( 'theme', get_stylesheet_directory_uri() . '/_app/js/theme.js' );
 		wp_localize_script( 'theme', 'MSWObject', array(
 			'ajax_url' => admin_url( 'admin-ajax.php' ),
 			'site_url' => site_url(),
 			'stylesheet_directory' => get_stylesheet_directory_uri()
 		) );
 
-		wp_enqueue_style( 'theme', get_stylesheet_uri() );
+		wp_enqueue_style( 'theme', get_stylesheet_directory_uri() . '/_app/css/style.css' );
 	}
 
 }
 
 $Makespace = new Makespace();
+
+function sc_button($atts, $content = null) {
+	extract( shortcode_atts( array(
+		'url' => '#',
+		'target' => '_self'
+	), $atts ) );
+	return '<a class="button primary" href="'. $url .'" target="'. $target .'">' . do_shortcode($content) . '</a>';
+}
+add_shortcode('button', 'sc_button');
